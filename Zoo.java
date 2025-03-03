c9588d5a03fbd3bcf347b53ba68231704064b3e8
Fritz Gerald Sarabia <fritzgsarabia@gmail.com>
2025-03-02 23:58:40 -0500

    Update Zoo.java
    question 16 (zoo birds) solved

--- Zoo.java
+++ Zoo.java
@@ -14,16 +14,32 @@ public class Zoo
     public Zoo(){
         zooBirds.add(new Bird("blue","blue jay",2));
         zooBirds.add(new Bird("blue","red cardinal",1));
+        zooBirds.add(new Bird("red","red cardinal",1));
+        zooBirds.add(new Bird("blue","red cardinal",1));
+        zooBirds.add(new Bird("red","red cardinal",1));
+        zooBirds.add(new Bird("blue","red cardinal",1));
+        zooBirds.add(new Bird("red","red cardinal",1));
+        zooBirds.add(new Bird("blue","red cardinal",1));
     }
     
     int countBlueBirds(){
         return 0;
     }
+    public int countBlueBirds() {
+        int count = 0;
+        for (Bird bird : zooBirds) {
+            if (bird.getColor().equals("blue")) {
+                count++;
+            }
+        }
+        return count;
+    }
     
-    //BlueBirdCount = 0 
-    //search "blue"
-    //while(found)
-    //System.out.println("Blue birds:"+ BlueBirdCount++)
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+        System.out.println("Number of blue birds: " + zoo.countBlueBirds());
+    }
+    
 }
 
 
